9c3de437998bc3774b26c0f2d509926ac758b072
fhub-1 <flutterhub1.21.0@gmail.com>
2024-03-19 00:16:57 +0300

    adding clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { ClerkProvider } from "@clerk/nextjs";
+import { ClerkProvider, auth } from "@clerk/nextjs";
 
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
@@ -8,9 +8,26 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // auth().protect();
   return (
     <html lang="en">
-      <ClerkProvider>
+      <ClerkProvider
+        appearance={{
+          variables: { colorPrimary: "#000000" },
+          elements: {
+            formButtonPrimary:
+              "bg-black border border-black border-solid hover:bg-white hover:text-black",
+            socialButtonsBlockButton:
+              "bg-white border-gray-200 hover:bg-transparent hover:border-black text-gray-600 hover:text-black",
+            socialButtonsBlockButtonText: "font-semibold",
+            formButtonReset:
+              "bg-white border border-solid border-gray-200 hover:bg-transparent hover:border-black text-gray-500 hover:text-black",
+            membersPageInviteButton:
+              "bg-black border border-black border-solid hover:bg-white hover:text-black",
+            card: "bg-[#fafafa]",
+          },
+        }}
+      >
         <body>{children}</body>
         <PrismicPreview repositoryName={repositoryName} />
       </ClerkProvider>
